be178fe7116b14f27cf1ed3a5d7bd87fc2e275a4
Ancelad <i_am_pomidorchik@mail.ru>
2016-08-19 17:01:21 +0500

    Add Russian translation

---
+++ translations/harbour-maxvol-ru.ts
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="83"/>
+        <source>A simple app to set maximum volume.</source>
+        <translation>Простое приложение для настройки максимальной громкости.</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="39"/>
+        <source>Max Vol: </source>
+        <translation>Громкость: </translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="697"/>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="713"/>
+        <source>Max Vol</source>
+        <translation>Max Vol</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="717"/>
+        <source>Set maximum volume</source>
+        <translation>Настройте максимальную громкость</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="738"/>
+        <source>Restore default value</source>
+        <translation>Восстановить значения</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="749"/>
+        <source>Apply after every reboot</source>
+        <translation>Применять после каждой перезагрузки</translation>
+    </message>
+</context>
+</TS>
